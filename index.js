8c9fe637d3db75168dd5a6ed654f22ae3c233670
Neeraj Belsare <belsareneeraj@gmail.com>
2024-02-09 00:44:48 +0530

    Fix MongoDB connection compatability

--- index.js
+++ index.js
@@ -5,14 +5,32 @@ const mongoose  = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
-mongoose.connect(process.env.DATABASEURL).then(()=>{
-    console.log("DATABASE Connected");
-}).catch((e)=>{
-    console.log('DATABASE Not Connected' + e.message);  
-})
-const port = process.env.PORT || 8080;
+// mongoose.connect(process.env.DATABASEURL).then(()=>{
+//     console.log("DATABASE Connected");
+// }).catch((e)=>{
+//     console.log('DATABASE Not Connected' + e.message);
+// })
+// const port = process.env.PORT || 8080;
 
+const PORT = process.env.PORT || 8080
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DATABASEURL);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
  
-app.listen(port,()=>{
-    console.log("Server is Running on " + port);
-});
+// app.listen(port,()=>{
+//     console.log("Server is Running on " + port);
+// });
+
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
